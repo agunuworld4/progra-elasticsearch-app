b8ff2e3a16228b827bbcf329270f5372859fc1b4
agunuworld4 <agunueghosa@gmail.com>
2025-01-06 13:21:15 -0600

    push

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# prometheus-grafana-monitoring
+# pgrykubernetes-ingress
--- helm-ingress-installed.sh
+++ helm-ingress-installed.sh
@@ -3,29 +3,30 @@ curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-sleep 30
+sleep 2
+ls -lart 
 #clone repo on server
-git clone git@github.com:agunuworld4/pgrykubernetes-ingress.git
+#git clone git@github.com:agunuworld4/pgrykubernetes-ingress.git
 #sleep 10
 #git checkout
 #
 #cd kubernetes-ingress/deployments
 sleep 10
 ## 2. Create a Namespace And SA
-kubectl apply -f kubernetes-ingress/deployments/common/ns-and-sa.yaml
+kubectl apply -f deployments/common/ns-and-sa.yaml
 
 sleep 10
 ##
-kubectl apply -f kubernetes-ingress/deployments/common/
+kubectl apply -f deployments/common/
 
 sleep 10
 ### 4. Deploy the Ingress Controller
 #DaemonSet*. Use a DaemonSet for deploying the Ingress controller on every node or a subset of nodes.
-kubectl apply -f kubernetes-ingress/deployments/daemon-set/nginx-ingress.yaml
+kubectl apply -f deployments/daemon-set/nginx-ingress.yaml
 
 sleep 10
 #For AWS, run:
-kubectl apply -f kubernetes-ingress/deployments/service/loadbalancer-aws-elb.yaml
+kubectl apply -f deployments/service/loadbalancer-aws-elb.yaml
 
 #get the DNS name of the ELB, run
 kubectl describe svc nginx-ingress --namespace=nginx-ingress
