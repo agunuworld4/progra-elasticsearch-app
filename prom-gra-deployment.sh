472b333a81ff67721aa54803e37ba68ffcbbea78
agunuworld4 <agunueghosa@gmail.com>
2025-01-06 13:34:55 -0600

    push

--- prom-gra-deployment.sh
+++ prom-gra-deployment.sh
@@ -5,17 +5,17 @@ sleep 40
 #Add grafana helm charts
 helm repo add grafana https://grafana.github.io/helm-charts
 sleep 10
-kubectl create ns monitoring
-#helm upgrade --install prometheus  prometheus/prometheus -n monitoring -f prometheus_values.yml
+kubectl create ns ibm-ucd
+#helm upgrade --install prometheus  prometheus/prometheus -n ibm-ucd -f prometheus_values.yml
 ls -lart
-helm upgrade --install prometheus  prometheus/prometheus -n monitoring -f alertmanager_rule_11_9_24.yml
+helm upgrade --install prometheus  prometheus/prometheus -n ibm-ucd -f alertmanager_rule_11_9_24.yml
 
 sleep 40
 
 #Grafana
-helm upgrade --install  grafana grafana/grafana  -n monitoring
+helm upgrade --install  grafana grafana/grafana  -n ibm-ucd
 
 sleep 40
-kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+kubectl get secret --namespace ibm-ucd grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
 kubectl apply -f ingresss_rules.yml
