12fb4e32a996537e9691fd8a900030174a95461f
agunuworld4 <agunueghosa@gmail.com>
2025-01-22 21:39:52 -0600

    jjva-ns-svc-pod

--- prom-gra-deployment.sh
+++ prom-gra-deployment.sh
@@ -5,17 +5,17 @@ sleep 40
 #Add grafana helm charts
 helm repo add grafana https://grafana.github.io/helm-charts
 sleep 10
-kubectl create ns ibm-ucd
-#helm upgrade --install prometheus  prometheus/prometheus -n ibm-ucd -f prometheus_values.yml
+kubectl create ns jjva-ns-svc-pod
+#helm upgrade --install prometheus  prometheus/prometheus -n jjva-ns-svc-pod -f prometheus_values.yml
 ls -lart
-helm upgrade --install prometheus  prometheus/prometheus -n ibm-ucd -f alertmanager_rule_11_9_24.yml
+helm upgrade --install prometheus  prometheus/prometheus -n jjva-ns-svc-pod -f alertmanager_rule_11_9_24.yml
 
 sleep 40
 
 #Grafana
-helm upgrade --install  grafana grafana/grafana  -n ibm-ucd
+helm upgrade --install  grafana grafana/grafana  -n jjva-ns-svc-pod
 
 sleep 40
-kubectl get secret --namespace ibm-ucd grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+kubectl get secret --namespace jjva-ns-svc-pod grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
 kubectl apply -f ingresss_rules.yml
